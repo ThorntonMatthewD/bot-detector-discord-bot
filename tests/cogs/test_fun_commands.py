a46d31b16e3481ccc4506f4185215c36c0f79ecb
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-07-07 21:23:00 -0400

    Adjust bot fixture

--- tests/cogs/test_fun_commands.py
+++ tests/cogs/test_fun_commands.py
@@ -1,16 +1,19 @@
+import asyncio
 import pytest
 import discord.ext.test as dpytest
 
 from conftest import test_bot
 from src.cogs.fun_commands import funCommands
 
-@pytest.fixture
+
+@pytest.fixture(autouse=True)
 async def set_up_bot(test_bot):
     await test_bot.add_cog(funCommands(test_bot))
-    yield test_bot
+    dpytest.configure(set_up_bot)
+    return test_bot
+
 
 @pytest.mark.asyncio
-async def test_poke(set_up_bot):
-    dpytest.configure(set_up_bot)
-    await dyptest.message("!poke")
+async def test_poke():
+    await dpytest.message("!poke")
     assert dpytest.verify().message().contains().content("Teehee")
--- tests/conftest.py
+++ tests/conftest.py
@@ -4,9 +4,20 @@ import pytest
 
 from discord.ext.commands import Bot
 
+
 @pytest.fixture
-async def test_bot():
-    bot: discord.Client = Bot(command_prefix='!')
+async def test_bot(event_loop):
+    bot: discord.Client = Bot(
+        command_prefix="!",
+        loop=event_loop,
+        intents=discord.Intents(
+            messages=True,
+            guilds=True,
+            members=True,
+            reactions=True,
+            message_content=True,
+        ),
+    )
     async with aiohttp.ClientSession() as session:
         bot.Session = session
 
